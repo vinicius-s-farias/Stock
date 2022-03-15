313f69bd0db5c4c7fd2c045c39e44678fe8adfc7
vinicius-s-farias <vifarias47@gmail.com>
2022-03-15 16:09:34 -0300

    quase

--- src/main/java/com/rest/api/ApiApplication.java
+++ src/main/java/com/rest/api/ApiApplication.java
@@ -6,44 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.security.Principal;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 
 @SpringBootApplication
 public class ApiApplication {
-	Random rand = new Random();
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-	}
-
-	@RestController
-	@CrossOrigin
-	class CaffeineLevelRestController {
-
-		String getCaffeineLevel() {
-			List<String> givenList = Arrays.asList(
-					"Head on table asleep. Needs coffee now!",
-					"Not at all. What's wrong?!",
-					"Mildly. Boring.",
-					"Making progress.",
-					"Everything is awesome. Stuff is definitely happening.",
-					"Eyeballs are rolling around in my head and I'm shouting at my coworker about JHipster.",
-					"The LD50 of caffeine is 100 cups. Your developer has had 99 and is talking to the bike rack outside while jogging in place.");
-
-			return givenList.get(rand.nextInt(givenList.size()));
-		}
-
-		@GetMapping("/howcaffeinatedami")
-		public String getCaffeineLevel(Principal principal) {
-			String userName = principal != null ? principal.getName() : "Anonymous";
-			return userName + ", your developer's caffeine level is: " + getCaffeineLevel();
-		}
 	}
 
 	@Configuration
