cd274d35aa6e451c42b8fc1d06d75dce0eb3aacd
vinicius-s-farias <vifarias47@gmail.com>
2022-03-18 16:32:18 -0300

    cross

--- src/main/java/com/rest/api/controller/GraficoController.java
+++ src/main/java/com/rest/api/controller/GraficoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:8082")
 @RestController
 public class GraficoController {
 
